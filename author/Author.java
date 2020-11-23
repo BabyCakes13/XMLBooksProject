eff078be5ff9e17e37e8718372c68e72ad8e5d01
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-11-23 19:17:04 +0200

    Added additional info for authors. Added genre.

--- author/Author.java
+++ author/Author.java
@@ -9,13 +9,19 @@ import java.util.ArrayList;
 public class Author {
   private int id;
   private String name;
+  private int birthYear;
+  private int deathYear;
+  private String nationality;
 
   public Author() {}
 
-  public Author(int id, String name) {
+  public Author(int id, String name, int birthYear, int deathYear, String nationality) {
     super();
     this.id = id;
     this.name = name;
+    this.birthYear = birthYear;
+    this.deathYear = deathYear;
+    this.nationality = nationality;
   }
 
   @XmlAttribute
@@ -36,8 +42,39 @@ public class Author {
     this.name = name;
   }
 
+  @XmlElement
+  public int getBirthYear() {
+    return this.birthYear;
+  }
+
+  public void setBirthYear(int birthYear) {
+    this.birthYear = birthYear;
+  }
+
+  @XmlElement
+  public int getDeathYear() {
+    return this.deathYear;
+  }
+
+  public void setDeathYear(int deathYear) {
+    this.deathYear = deathYear;
+  }
+
+  @XmlElement
+  public String getNationality() {
+    return this.nationality;
+  }
+
+  public void setNationality(String nationality) {
+    this.nationality = nationality;
+  }
+
   @Override
     public String toString() {
-        return "Author [" + this.id + "]: " + this.name;
+        return "Author [" + this.id + "]: " +
+                            this.name + " (" +
+                            this.birthYear + " - " +
+                            this.deathYear + "), " +
+                            this.nationality;
     }
 }
